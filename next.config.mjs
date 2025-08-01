dd47e3079105a6497695888c7360566b7093fb29
Aniket-workspace <124556885+Aniket-workspace@users.noreply.github.com>
2025-08-01 17:50:23 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,19 @@ const nextConfig = {
   images: {
     domains: ['cdn.dummyjson.com'], // Add the external hostname here
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all routes
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
